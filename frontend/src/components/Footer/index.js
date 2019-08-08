8ed550b5cd947695d45d96e16177b2d050433ffa
choiyeonseok <smr603@snu.ac.kr>
2019-08-09 05:43:33 +0900

    자동번역

--- frontend/src/components/Footer/index.js
+++ frontend/src/components/Footer/index.js
@@ -1,4 +1,5 @@
 import React from "react";
+import PropTypes from 'prop-types';
 import "./styles.scss";
 
 const Footer = (props, context) => (
@@ -6,16 +7,16 @@ const Footer = (props, context) => (
         <div className="column">
             <nav className="nav">
                 <ul className="list">
-                    <li className="list-item">About Us</li>
-                    <li className="list-item">Support</li>
-                    <li className="list-item">Blog</li>
-                    <li className="list-item">Press</li>
-                    <li className="list-item">API</li>
-                    <li className="list-item">Jobs</li>
-                    <li className="list-item">Privacy</li>
-                    <li className="list-item">Terms</li>
-                    <li className="list-item">Directory</li>
-                    <li className="list-item">Language</li>
+                    <li className="list-item">{context.t("About Us")}</li>
+                    <li className="list-item">{context.t("Support")}</li>
+                    <li className="list-item">{context.t("Blog")}</li>
+                    <li className="list-item">{context.t("Press")}</li>
+                    <li className="list-item">{context.t("API")}</li>
+                    <li className="list-item">{context.t("Jobs")}</li>
+                    <li className="list-item">{context.t("Privacy")}</li>
+                    <li className="list-item">{context.t("Terms")}</li>
+                    <li className="list-item">{context.t("Directory")}</li>
+                    <li className="list-item">{context.t("Language")}</li>
                 </ul>
             </nav>
         </div>
@@ -23,6 +24,10 @@ const Footer = (props, context) => (
             <span className="copyright">&copy; 2017 nomadgram</span>
         </div>
     </footer>
-)
+);
+
+Footer.contextTypes = {
+    t: PropTypes.func.isRequired
+};
 
 export default Footer;
--- frontend/src/translations.js
+++ frontend/src/translations.js
@@ -1,8 +1,16 @@
 export const translations = {
-    es: {
-        "Login in": "Insiciar Sesin"
-    },
-    kr: {
-        "Log in" : "로그인"
-    }
-};
+  'ko': {
+    'About Us': '인스타그램',
+    'Support': '지원',
+    'Blog': '블로그',
+    'Press': '출판',
+    'API': '에이피아이',
+    'Jobs': '직업',
+    'Privacy': '개인정보',
+    'Terms': '약관',
+    'Directory': '디렉토리',
+    'Language': '언어',
+  },
+  'options': {
+  },
+}

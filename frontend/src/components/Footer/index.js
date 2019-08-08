ba4dd10bd9877c8d1f811336bc0bd23f4ceef45c
choiyeonseok <smr603@snu.ac.kr>
2019-08-09 02:23:12 +0900

    footer component 추가

---
+++ frontend/src/components/Footer/index.js
@@ -0,0 +1,28 @@
+import React from "react";
+import "./styles.scss";
+
+const Footer = (props, context) => (
+    <footer className="footer">
+        <div className="column">
+            <nav className="nav">
+                <ul className="list">
+                    <li className="list-item">About Us</li>
+                    <li className="list-item">Support</li>
+                    <li className="list-item">Blog</li>
+                    <li className="list-item">Press</li>
+                    <li className="list-item">API</li>
+                    <li className="list-item">Jobs</li>
+                    <li className="list-item">Privacy</li>
+                    <li className="list-item">Terms</li>
+                    <li className="list-item">Directory</li>
+                    <li className="list-item">Language</li>
+                </ul>
+            </nav>
+        </div>
+        <div className="column">
+            <span className="copyright">&copy; 2017 nomadgram</span>
+        </div>
+    </footer>
+)
+
+export default Footer;
--- frontend/src/redux/configureStore.js
+++ frontend/src/redux/configureStore.js
@@ -3,14 +3,14 @@ import thunk from "redux-thunk";
 import users from "./modules/users";
 import { connectRouter } from 'connected-react-router';
 import { routerMiddleware } from "react-router-redux";
-import createHistory from "history/createBrowserHistory";
+import * as createHistory from "history";
 import { composeWithDevTools } from "redux-devtools-extension";
 import { i18nState } from "redux-i18n";
 
 
 const env = process.env.NODE_ENV;
 
-const history = createHistory();
+const history = createHistory.createBrowserHistory();
 
 const middlewares = [thunk, routerMiddleware(history)];
 //list로 구성된 thunk를 middleware 로 받아서 unpack함. 

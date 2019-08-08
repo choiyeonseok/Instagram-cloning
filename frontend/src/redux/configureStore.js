8d491cc661a008fc7661719efdc29c035f17df94
choiyeonseok <smr603@snu.ac.kr>
2019-08-09 01:08:51 +0900

    reducer and routing navigation and history sync

--- frontend/src/redux/configureStore.js
+++ frontend/src/redux/configureStore.js
@@ -1,25 +1,33 @@
 import { createStore, combineReducers, applyMiddleware} from "redux";
 import thunk from "redux-thunk";
 import users from "./modules/users";
+import { connectRouter } from 'connected-react-router';
+import { routerMiddleware } from "react-router-redux";
+import createHistory from "history/createBrowserHistory";
 
 const env = process.env.NODE_ENV;
 
-const middlewares = [thunk];
+const history = createHistory();
+
+const middlewares = [thunk, routerMiddleware(history)];
 //list로 구성된 thunk를 middleware 로 받아서 unpack함. 
+
 
 if(env ==='development'){
     const { logger } = require("redux-logger"); //dev일때만 logger를 부른다.
     middlewares.push(logger);  // middleware는 현재 array
-}else {
-
 }
 
 const reducer = combineReducers({
-    users
-})
+    users,
+    router: connectRouter(history),
+});
 
-let store = initialState => createStore(reducer, applyMiddleware(...middlewares));
+let store = initialState => 
+    createStore(reducer, applyMiddleware(...middlewares));
 
+
+export { history };
 
 export default store();
 

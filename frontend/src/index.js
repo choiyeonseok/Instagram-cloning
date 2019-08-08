172c83bc40c201b655416384524af8201ec96aac
choiyeonseok <smr603@snu.ac.kr>
2019-08-08 17:52:12 +0900

    create-react-app frontend'

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,29 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from "react-redux";
-import store, { history } from "./redux/configureStore";
-import { ConnectedRouter } from "connected-react-router";
-import App from './components/App';
-import I18n from 'redux-i18n';
-import { translations } from "./translations";
-//import "./ReactotronConfig";
+import './index.css';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
 
-const rootElement = document.getElementById("root");
-
-ReactDOM.render(
-    <Provider store={store}>
-        <ConnectedRouter history={history}>
-            <I18n translations={translations} initialLang="en" fallbackLang="en">
-                <App className="app"/> 
-            </I18n>
-        </ConnectedRouter>
-    </Provider>, 
-    rootElement
-);
+ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-
-
-//localStorage.setItem('bestCourse', 'nomad academy');
+serviceWorker.unregister();

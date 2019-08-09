56d17acb73d39a1eaf70908e709adb9361852a22
choiyeonseok <smr603@snu.ac.kr>
2019-08-09 23:11:09 +0900

    like/unlike photo at front

--- frontend/src/redux/modules/photos.js
+++ frontend/src/redux/modules/photos.js
@@ -5,6 +5,9 @@ import { actionCreators as userActions } from "../modules/user";
 // actions
 
 const SET_FEED = "SET_FEED";
+const LIKE_PHOTO = "LIKE_PHOTO";
+const UNLIKE_PHOTO = "UNLIKE_PHOTO";
+
 
 // action creators
 
@@ -12,6 +15,20 @@ function setFeed(feed){
     return {
         type: SET_FEED,
         feed
+    }
+}
+
+function doLikePhoto(photoId){
+    return {
+        type: LIKE_PHOTO,
+        photoId
+    }
+}
+
+function doUnlikePhoto(photoId) {
+    return {
+        type: UNLIKE_PHOTO,
+        photoId
     }
 }
 
@@ -35,6 +52,11 @@ function getFeed(){
     }
 }
 
+function likePhoto(photoId) {
+    return (dispatch, getState) => {
+        dispatch(doLikePhoto(photoId))   // 즉각 색칠액션을 보낸다.
+    }
+}
 
 // initial state
 
@@ -45,7 +67,11 @@ const initialState = {};
 function reducer(state=initialState, action){
     switch(action.type){
         case SET_FEED:
-            return applySetFeed(state, action);
+            return applySetFeed(state, action); 
+        case LIKE_PHOTO:
+            return applyLikePhoto(state, action);
+        case UNLIKE_PHOTO:
+            return applyUnlikePhoto(state, action);
         default:
             return state
     }
@@ -61,6 +87,30 @@ function applySetFeed(state, action){
     }
 }
 
+function applyLikePhoto(state, action){
+    const { photoId } = action;
+    const { feed } = state;
+    const updatedFeed = feed.map(photo => {
+        if( photo.id === photoId ){
+            return {...photo, is_liked: true , like_count: photo.like_count + 1}
+        }
+        return photo // 무변화
+    });
+    return { ...state, feed: updatedFeed }
+
+}
+
+function applyUnlikePhoto(state, action) {
+    const { photoId } = action;
+    const { feed } = state;
+    const updatedFeed = feed.map(photo => {
+        if (photo.id === photoId) {
+            return { ...photo, is_liked: false , like_count: photo.like_count - 1}
+        }
+        return photo // 무변화
+    });
+    return {...state, feed: updatedFeed}
+}
 
 // exports
 

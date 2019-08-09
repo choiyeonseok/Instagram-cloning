ca5a1500b87aaa5538ff98daa5858457b4ee9ac3
choiyeonseok <smr603@snu.ac.kr>
2019-08-09 15:06:35 +0900

    Enroll the Navigation app

--- frontend/src/components/App/presenter.js
+++ frontend/src/components/App/presenter.js
@@ -4,10 +4,10 @@ import { Route, Switch } from "react-router-dom";
 import './styles.scss';
 import Footer from '../Footer';
 import Auth from '../Auth';
+import Navigation from "../Navigation";
 
 const App = props => [
-
-    //Nav,
+    props.isLoggedIn ? <Navigation key={1} /> : null,
     props.isLoggedIn ? <PrivateRoutes key={2} /> : <PublicRoutes key={2} />,
     <Footer key={3} />
 ];
